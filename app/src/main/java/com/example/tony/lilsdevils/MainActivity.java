4412cf44c5bb29bf5ea7d4d154d2c7cb5a689370
Tony <apeloza@alumni.uwo.ca>
2018-11-03 16:39:17 -0500

    Initial commit. No idea what should be in gitignore, lol.

---
+++ app/src/main/java/com/example/tony/lilsdevils/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.tony.lilsdevils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void summonDevil(View view){
+        int id = getResources().getIdentifier("summon_face", "drawable", getPackageName());
+        ImageView devilImage = findViewById(R.id.DevilImage);
+        devilImage.setImageResource(id);
+    }
+}
